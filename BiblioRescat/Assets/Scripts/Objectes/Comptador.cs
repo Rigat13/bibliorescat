87320f292c4ac4db64d9b24740f3ee270f54898b
Carles Rigat Almansa <carlesrigat@gmail.com>
2024-04-03 20:28:26 +0200

    Addició del Tauler de Missions i els seus panells
    També s'ha canviat el nom de la carpeta Animations a Animacions.

--- BiblioRescat/Assets/Scripts/Objectes/Comptador.cs
+++ BiblioRescat/Assets/Scripts/Objectes/Comptador.cs
@@ -4,10 +4,12 @@ public class Comptador : MonoBehaviour
 {
     public int llibres = 0;
     public TMP_Text comptadorLlibres;
+    public TaulerMissio taulerMissio;
 
     public void AfegirLlibre()
     {
         llibres++;
         comptadorLlibres.text = llibres.ToString();
+        taulerMissio.AvisarLlibreTrobat();
     }
 }
---
+++ BiblioRescat/Assets/Scripts/Objectes/TaulerMissio.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TaulerMissio : MonoBehaviour
+{
+    [Header("Objectiu")]
+    public int objectiuLlibres = 3;
+    public Comptador comptador;
+
+    [Header("Panells")]
+    public GameObject alerta;
+    public Animation panellMissioEnCurs;
+    public Animation panellMissioComplerta; 
+    public TMP_Text comptadorLlibresPanellEnCurs, comptadorLlibresPanellComplerta;
+
+    void Start()
+    {
+        alerta.SetActive(false); // S'oculta el senyal d'alerta
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Jugador")) {
+            if (comptador.llibres >= objectiuLlibres) {
+                ObrirPanellMissioComplerta();
+            } else {
+                ObrirPanellMissioEnCurs();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Jugador")) {
+            if (comptador.llibres >= objectiuLlibres) {
+                TancarPanellMissioComplerta();
+            } else {
+                TancarPanellMissioEnCurs();
+            }
+        }
+    }
+
+    public void AvisarLlibreTrobat() {
+        if (comptador.llibres >= objectiuLlibres) {
+            alerta.SetActive(true); // S'activa el senyal d'alerta
+        }
+    }
+
+    public void ObrirPanellMissioEnCurs() {
+        comptadorLlibresPanellEnCurs.text = comptador.llibres + " / " + objectiuLlibres;
+        panellMissioEnCurs.gameObject.SetActive(true);
+        panellMissioEnCurs.Play("ObrirPanell");
+    }
+
+    public void TancarPanellMissioEnCurs() {
+        panellMissioEnCurs.gameObject.SetActive(false);
+        panellMissioEnCurs.Play("TancarPanell");
+    }
+
+    public void ObrirPanellMissioComplerta() {
+        comptadorLlibresPanellComplerta.text = comptador.llibres + " / " + objectiuLlibres;
+        panellMissioComplerta.gameObject.SetActive(true);
+        panellMissioComplerta.Play("ObrirPanell");
+    }
+
+    public void TancarPanellMissioComplerta() {
+        panellMissioComplerta.gameObject.SetActive(false);
+        panellMissioComplerta.Play("TancarPanell");
+    }
+
+    public void ConfirmarViatge() {
+        TancarPanellMissioComplerta();
+        Debug.Log("Viatge confirmat!");
+    }
+}
